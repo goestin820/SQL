8134689706fde88b36c961bcbc4b2e1cd6c0be30
goestin <goestin@gmail.com>
2023-05-19 16:25:07 +0800

    20230519-VOTE投票系統基本功能建立

--- vote/tin/backend.php
+++ vote/tin/backend.php
@@ -1,28 +1,118 @@
+<?php include_once "db.php";?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>管理後台</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <style>
+        *{
+    box-sizing: border-box;
+    margin:0;
+    padding:0;
+}
+.time-set{
+    display:flex;
+    width:70%;
+    min-width:615px;
+    justify-content: space-between;
+}
+.time-item{
+    margin:5px 0;
+}
+.subject-input{
+    font-family: '微軟正黑體',Arial, Helvetica, sans-serif;
+    padding:3px;
+    font-size:16px;
+    width:50%;
+}
+main{
+    width:60%;
+    margin:auto;
+}
+
+.topic-list{
+    list-style-type: none;
+}
+.list-row{
+    display:flex;
+    width:100%;
+}
+.list-row div:not(1){
+    text-align: center;
+}
+.list-item-title:nth-child(1),
+.list-item:nth-child(1){
+    width:40%;
+}
+
+.list-item-title:nth-child(2),
+.list-item:nth-child(2){
+    width:5%;
+}
+
+.list-item-title:nth-child(3),
+.list-item:nth-child(3){
+    width:15%;
+}
+
+.list-item-title:nth-child(4),
+.list-item:nth-child(4){
+    width:5%;
+}
+.list-item-title:nth-child(5),
+.list-item:nth-child(5){
+    width:25%;
+}
+    </style>
 </head>
-
 <body>
-    <header>
-        <a href="index.php">首頁</a>
-        <a href="logout.php">登出</a>
-    </header>
-    <main>
-        <a href="./back/add_vote.php">新增投票</a>
-        <a href="./back/edit_vote.php">編輯投票</a>
-        <a href="./back/del_vote.php">刪除投票</a>
-        <a href="./back/query_vote.php">結果查詢</a>
-        <a href=""></a>
-    </main>
-    <footer>
+<header>
+    <a href="index.php">首頁</a>
+    <a href="login.php">登出</a>
+    <nav>
+        <a href='./backend.php?do=add_vote'>新增投票</a>     
+        <a href='./backend.php?do=query_vote'>結果查詢</a>
+    </nav>
+</header>
+<main>
+<?php
+ //include "./back/topic_list.php";
 
-    </footer>
+/*  if(isset($_GET['do'])){
+    $file="./back/".$_GET['do'].".php";
+ }else{
+    $file="./back/topic_list.php";
+ } */
+
+//$do=(isset($_GET['do']))?$_GET['do']:'topic_list';
+$do=$_GET['do']??'topic_list';
+
+$file="./back/".$do.".php";
+
+if(file_exists($file)){
+    include $file;
+}else{
+    include "./back/topic_list.php";
+}
+
+//include (file_exists($file))?$file:"./back/topic_list.php";
+ /* 
+ switch($_GET['do']){
+    case "add_vote":
+        include "./back/add_vote.php";
+    break;
+    case 'query_vote':
+        include "./back/query_vote.php";
+    break;
+    default :
+        include "./back/topic_list.php";
+ } */
+ ?>
+</main>
+<footer></footer>
 </body>
-
 </html>
--- vote/tin/index.php
+++ vote/tin/index.php
@@ -1,41 +1,36 @@
+<?php include_once "db.php";?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>驚奇投票所</title>
 </head>
-
 <body>
-    <header>
-        <a href="index.php">首頁</a>
-        <a href="/front/result.php">結果</a>
-        <a href="/front/login.php">登入</a>
-        <a href="/front/reg.php">註冊</a>
-    </header>
+<header>
+    <a href="index.php">首頁</a>
+    <a href="index.php?do=result">結果</a>
+    <a href="index.php?do=login">登入</a>
+    <a href="index.php?do=reg">註冊</a>
+</header>
+<main>
 
-    <main>
-        <ul>
-            <?php
-            //資料庫連線
-            include_once "db.php";
+<?php
 
-            $sql = "select * from `topics`";
-            $rows = $pdo->query($sql)->fetchAll();
-            foreach ($rows as $row) {
-            ?>
-                <li><?= $row['subject']; ?></li>
-            <?php
-            }
-            ?>
-        </ul>
-    </main>
+$do=$_GET['do']??'list';
 
-    <footer>
+$file="./front/".$do.".php";
 
-    </footer>
+if(file_exists($file)){
+    include $file;
+}else{
+    include "./front/list.php";
+}
+?>
+
+</main>
+<footer></footer>
+
 </body>
-
 </html>
