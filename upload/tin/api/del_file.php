c719e45e770626d127a27e4e380162c5561f154f
goestin <goestin@gmail.com>
2023-08-24 16:21:12 +0800

    20230824-文字檔案處理csv

--- upload/tin/api/del_file.php
+++ upload/tin/api/del_file.php
@@ -1,12 +1,12 @@
 <?php
-$dsn = "mysql:host=localhost;charset=utf8;dbname=upload";
-$pdo = new PDO($dsn, 'root', '');
+$dsn="mysql:host=localhost;charset=utf8;dbname=upload";
+$pdo=new PDO($dsn,'root','');
 
-$sql = "delete from `images` where `id`='{$_GET['id']}'";
+$sql="delete from `images` where `id`='{$_GET['id']}'";
 
-$img = $pdo->query("select `img` from `images` where `id`='{$_GET['id']}'")->fetch(PDO::FETCH_ASSOC);
+$img=$pdo->query("select `img` from `images` where `id`='{$_GET['id']}'")->fetch(PDO::FETCH_ASSOC);
 
-unlink("../img/" . $img['img']);
+unlink("../img/".$img['img']);
 
 $pdo->exec($sql);
 
--- upload/tin/text-import.php
+++ upload/tin/text-import.php
@@ -1,3 +1,17 @@
+<style>
+    table {
+        border: 1px solid #ccc;
+        box-shadow: 3px 3px 15px #aaa;
+        padding: 20px;
+        border-collapse: collapse;
+    }
+
+    td {
+        border: 1px solid #ccc;
+        padding: 5px 10px;
+        text-align: center;
+    }
+</style>
 <?php
 /****
  * 1.建立資料庫及資料表
@@ -9,7 +23,41 @@
  * 7.結束檔案
  */
 
+if (!empty($_FILES['text']['tmp_name'])) {
+    echo "檔名是:" . $_FILES['text']['name'];
+    echo "<br>";
+    echo "檔案大小是:" . $_FILES['text']['size'];
+    echo "<hr>";
+
+    move_uploaded_file($_FILES['text']['tmp_name'], "./document/{$_FILES['text']['name']}");
+    $path = "./document/{$_FILES['text']['name']}";
+    // 使用 'r' 模式來讀取檔案
+    $file = fopen($path, "r");
+    echo "<table>";
+
+    if ($file) {
+        // 讀取檔案到最後
+        while (($line = fgets($file)) !== false) {
+            echo "<tr>";
+            $cols = explode(",", $line);
+            for ($i = 0; $i < count($cols); $i++) {
+                echo "<td>";
+                echo $cols[$i];
+                echo "</td>";
+            }
+
+            echo "</tr>";
+        }
+        fclose($file);
+    } else {
+        // 檔案開啟失敗
+        echo "檔案開啟失敗";
+    }
+
+    echo "</table>";
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,15 +67,14 @@
     <title>文字檔案匯入</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<h1 class="header">文字檔案匯入練習</h1>
-<!---建立檔案上傳機制--->
-
-
-
-<!----讀出匯入完成的資料----->
-
-
-
+    <h1 class="header">文字檔案匯入練習</h1>
+    <!---建立檔案上傳機制--->
+    <form action="?" method="post" enctype="multipart/form-data">
+        <input type="file" name="text" id="text">
+        <input type="submit" value="上傳">
+    </form>
+    <!----讀出匯入完成的資料----->
 </body>
 </html>
